e741ef751859d8b7d30df1beed64f2a3103d8aab
Tatsuya Kitabatake <e2tod2@gmail.com>
2016-07-18 01:49:20 +0900

    define tmp state

--- reducers/index.jsx
+++ reducers/index.jsx
@@ -1,5 +1,27 @@
 import { combineReducers } from 'redux'
 
+// all state
+
+// {
+//   teban: SENTE or GOTE,
+//   sente: {
+//     motigoma: {
+//       {
+//         id: ,
+//         position: ,
+//         type: ,
+//         nari: boolean
+//       }
+//     },
+//     banjyou: {
+
+//     }
+//   },
+//   gote: {},
+
+//   UI STATE
+// }
+
 const reducer = combineReducers({})
 
 export default reducer
